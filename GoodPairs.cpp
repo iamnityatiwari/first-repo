e8b35b2cd0c04e491beae12ce258827f6f2404fd
iamnityatiwari <iamnityatiwari2400@gmail.com>
2024-01-06 18:15:21 +0530

    2nd day potd

---
+++ GoodPairs.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+ int numIdenticalPairs(vector<int>& nums) {
+        int res = 0 ;
+        for (int i=0; i<nums.size(); i++){
+            for (int j=0; j<nums.size(); j++){
+                if (nums[i]==nums[j] && i<j){
+                    res++ ;
+                }
+            }
+        }
+        return res ;
+    }
+
+int main()
+{
+    vector<int>nums{1,2,3,1,1,3};
+    cout<<numIdenticalPairs(nums);
+}
